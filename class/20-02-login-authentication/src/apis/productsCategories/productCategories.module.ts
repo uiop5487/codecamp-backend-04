8cc1ef2f3c777dfaf7791e9af1ab081623365d5c
uiop5487 <uiop5487@gmail.com>
2022-07-31 21:25:31 +0900

    Update day21

--- class/20-02-login-authentication/src/apis/productsCategories/productCategories.module.ts
+++ class/20-02-login-authentication/src/apis/productsCategories/productCategories.module.ts
@@ -6,7 +6,7 @@ import { ProductCategoriesService } from './productCategories.service';
 
 @Module({
   imports: [
-    // typeorm 모듈은 엔티티를 전달한다. 없으면 데이터베이스와 연동이 안됨
+    // typeorm 모듈은 엔티티를 전달한다. 없으면 데이터베이스와 연동이 안됨.
     TypeOrmModule.forFeature([
       ProductCategory, //
     ]),
