3d1c8a755e32525b1d2e880c843da285521faa2c
uiop5487 <uiop5487@gmail.com>
2022-08-16 19:08:30 +0900

    Update day31

--- class/01-01-token/index.js
+++ class/01-01-token/index.js
@@ -1,13 +1,13 @@
 console.log("안녕하세요!");
 
-// 6자리만 나오는 토큰
-// const getToken = () => {
-//   const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
-//   console.log(result);
-//   return result;
-// };
+// // 6자리만 나오는 토큰
+// // const getToken = () => {
+// //   const result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+// //   console.log(result);
+// //   return result;
+// // };
 
-// getToken();
+// // getToken();
 
 // 인자로 받는 갯수 만큼에 자릿수가 나오는 토큰
 const getToken = (num) => {
